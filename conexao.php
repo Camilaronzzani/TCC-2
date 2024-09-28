e0a06d8a9814fe915a102cba402af4f4ec38b783
Camila Luiza Ronzzani da Silva <camilasiva2863@gmail.com>
2024-09-28 19:56:39 -0300

    conexao

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'tcc-2';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão realizada com sucesso!";
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
+?>
