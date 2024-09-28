f67561dba532f24a0635a9b4e7971dd0fe4675b0
bluexbaez <bluexbaez@gmail.com>
2024-09-28 20:08:17 -0300

    feito conf base, footer e nav

---
+++ nav.php
@@ -0,0 +1,16 @@
+<nav>
+  <div class="nav-left">
+    <a href="/">
+      <img src="imagens/logo.png" alt="Logo">
+    </a>
+  </div>
+  <div class="nav-center">
+    <a href="#">Adidas</a>
+    <a href="#">Nike</a>
+    <a href="#">Puma</a>
+  </div>
+  <div class="nav-right">
+    <a href="/login">Entrar</a>
+    <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/126/126510.png" alt="Carrinho"></a>
+  </div>
+</nav>
